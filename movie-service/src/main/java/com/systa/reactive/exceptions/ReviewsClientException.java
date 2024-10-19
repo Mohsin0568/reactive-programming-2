db3c7800b9636f4aa4349c00318c239ffaca7d85
Mohsin Murtuza <mohsinmurtuza@Mohsins-MacBook-Air.local>
2024-10-19 10:05:51 +0100

    added exception classes

--- movie-service/src/main/java/com/systa/reactive/exceptions/ReviewsClientException.java
+++ movie-service/src/main/java/com/systa/reactive/exceptions/ReviewsClientException.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 public class ReviewsClientException extends RuntimeException{
 
     private final String message;
+    private final Integer statusCode;
 
-    ReviewsClientException(final String message){
+    ReviewsClientException(final String message, final Integer statusCode){
         super(message);
         this.message = message;
+        this.statusCode = statusCode;
     }
 }
